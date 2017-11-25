3e166c49e738adafa1e29d7fb8f4535dfbb8e1e8
Eric Rogers <eric.rog17@gmail.com>
2017-11-25 00:13:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  var firstSelector = document.querySelector(selector)
+  return firstSelector
+};
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+};
+
+function getFirstSelector(selector) {
+  var firstSelector = document.querySelector(selector)
+  return firstSelector
+};
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+};
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('#app ul.ranked-list li')
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+};
